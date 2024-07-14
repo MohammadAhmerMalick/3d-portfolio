348d09e645ff1c13cb27259f3fef6eae98364254
unknown <mohammadahmermalick@gmail.com>
2024-07-14 12:34:37 +0400

    feat: scene,camera,renderer configrations added

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,22 @@
-import "./style.css"
+import { Scene, PerspectiveCamera, WebGLRenderer } from "three"
 
+import "./style.css"
 console.log("this is 3d portfolio")
+
+const scene = new Scene()
+
+const cameraRation = window.innerWidth / window.innerHeight
+const camera = new PerspectiveCamera(75, cameraRation, 0.0, 1000)
+camera.position.setZ(30)
+
+const canvas = document.querySelector("#bg")
+if (canvas) {
+  const renderer = new WebGLRenderer({
+    canvas,
+  })
+
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  renderer.render(scene, camera)
+}
